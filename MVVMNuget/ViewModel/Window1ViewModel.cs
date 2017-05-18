7876522a9240bc2fbaf427c2dfa2124c80f0dafa
Billly <lyy0808@yeah.net>
2017-05-18 14:50:00 +0800

    新建

---
+++ MVVMNuget/ViewModel/Window1ViewModel.cs
@@ -0,0 +1,56 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMNuget.ViewModel
+{
+    public static class MessageToken
+    {
+        /// <summary>
+        /// 动画信息标志
+        /// </summary>
+        public static readonly string AnimateMessageToken;
+
+        /// <summary>
+        /// 发送消息标志
+        /// </summary>
+        public static readonly string SendMessageToken;
+
+        static MessageToken()
+        {
+            AnimateMessageToken = nameof(AnimateMessageToken);
+
+            SendMessageToken = nameof(SendMessageToken);
+        }
+    }
+    public  class Window1ViewModel : ViewModelBase
+    {
+        public Window1ViewModel()
+        {
+            Messenger.Default.Register<string>(this, MessageToken.SendMessageToken, (msg) =>
+            {
+                Msg = msg;
+            });
+        }
+        private string _msg;
+
+        public string Msg
+        {
+            get
+            {
+                return _msg;
+            }
+            set
+            {
+                _msg = value;
+                RaisePropertyChanged(() => Msg);
+            }
+        }
+
+        
+    }
+}
